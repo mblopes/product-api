582848ea67acce8d29d9bd4407e1060592a06e60
Matheus B. Lopes <matheus.bachiste@olist.com>
2021-02-05 16:03:44 -0300

    Controllers, daos and models tests created

---
+++ src/tests/controllers/test_product_controller.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('.')
+
+import pytest
+
+from src.controllers.base_controller import BaseController
+from src.controllers.product_controller import ProductController
+from src.models.product import Product
+
+
+@pytest.fixture
+def create_instance():
+    product = ProductController()
+    return product
+
+
+def test_product_controller_instance(create_instance):
+    assert isinstance(create_instance, BaseController)
+    assert isinstance(create_instance, ProductController)
+
+
+def test_read_all_should_return_list(create_instance):
+    result = create_instance.read_all()
+    assert isinstance(result, list)
+
+
+def test_read_by_id_with_invalid_id_should_raise_exception(create_instance):
+
+    with pytest.raises(Exception) as exc:
+        create_instance.read_by_id(71289379)
+    assert str(exc.value) == 'Object not found in the database.'
